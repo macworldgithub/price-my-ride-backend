fbc1d1278b003b71a659a5690cd9fb34eb143ed8
Talal Zafar <talalzafar84@gmail.com>
2025-03-27 21:06:34 +0500

    .

--- routers/recordRoutes.js
+++ routers/recordRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { signup, login } = require("../controllers/authController");
 const { addRecord, getRecordById, getAllRecords, deleteRecord } = require("../controllers/RecordController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -11,15 +12,18 @@ router.post(
 
 router.get(
   "/getAll",
+  authMiddleware,
   getAllRecords
 );
 router.get(
   "/getRecordById",
+  authMiddleware,
   getRecordById
 );
 
 router.delete(
   "/delete",
+  authMiddleware,
   deleteRecord
 );
 
