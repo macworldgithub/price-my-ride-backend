3d1b89400578a00a6e1961f3e391a14c420292a0
jawwadkareem <karimjawwad09@gmail.com>
2026-01-20 12:34:01 +0500

    ..

--- routers/predictPriceRoutes.js
+++ routers/predictPriceRoutes.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const { PredictPrice } = require('../controllers/PredictPriceController');
+const express = require("express");
+const { PredictPrice } = require("../controllers/PredictPriceController");
 
 const PredictPriceRouter = express.Router();
 
-PredictPriceRouter.post('/predict/price', PredictPrice);
+PredictPriceRouter.post("/predict/price", PredictPrice);
 
 module.exports = PredictPriceRouter;
